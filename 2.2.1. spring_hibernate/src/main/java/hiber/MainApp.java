ec220d534f5959ef156044bfdf9cf826d090b01b
Traindriver-M <sergioparovozov@mail.ru>
2022-04-16 16:56:27 +0300

    Практическая задача 2.2.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -6,24 +6,19 @@ import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-
         UserService userService = context.getBean(UserService.class);
-
         Car hyundai = new Car("Hyundai", 2008);
         Car lada = new Car("Lada", 2109);
         Car uaz = new Car("UAZ", 2022);
-
         User user1 = new User("User1", "Lastname1", "user1@mail.ru");
         User user2 = new User("User2", "Lastname2", "user2@mail.ru");
         User user3 = new User("User3", "Lastname3", "user3@mail.ru");
-
         user1.setCar(hyundai);
         user2.setCar(lada);
         user3.setCar(uaz);
@@ -34,7 +29,6 @@ public class MainApp {
         userService.add(user2);
         userService.add(user3);
 
-
         //Выводит всех пользователей с машинами.
         List<User> users = userService.listUsers();
         for (User user : users) {
@@ -43,11 +37,10 @@ public class MainApp {
             System.out.println("Last Name = " + user.getLastName());
             System.out.println("Email = " + user.getEmail());
             System.out.println("Car = " + user.getCar());
-            System.out.println();
         }
-        // Выводит пользователя по марке и серии машины.
+        // Выводит пользователя по марке и серии машины или выводит сообщение если пользователя с такой машиной нет.
         System.out.println(userService.getUserByCar("hyundai", 2008));
-
+        System.out.println(userService.getUserByCar("lamba", 2005));
         context.close();
     }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -5,35 +5,42 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public User getUserByCar(String model, int series) {
-      String hql = "from User user where user.car.model = :model and user.car.series = :series";
-      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
-      query.setParameter("model", model).setParameter("series", series);
-      return query.setMaxResults(1).getSingleResult();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public User getUserByCar(String model, int series) {
+        User user = null;
+        String hql = "from User user where user.car.model = :model and user.car.series = :series";
+        Query query = sessionFactory.getCurrentSession().createQuery(hql);
+        query.setParameter("model", model).setParameter("series", series);
+        try {
+            user = (User) query.getSingleResult();
+        } catch (NoResultException nre) {
+            System.out.println("There is no user with such a car!");
+        }
+        return user;
+    }
 
 }
